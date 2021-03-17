0bb683f197913e52cf4d0fef7a62d8ea4f582199
Sam Stinson <samstinson@gmail.com>
2021-03-17 13:34:53 -0700

    Add project files.

---
+++ Patches.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using UnityEngine;
+
+namespace TLDQOL_Windproof
+{
+    class Patches
+    {
+        [HarmonyPatch(typeof(GameManager),"Awake")]
+
+        internal class GameManager_Awake
+        {
+            private static void Postfix()
+            {
+                MelonLoader.MelonLogger.Log("Hello World");
+            }
+        }
+    }
+}
